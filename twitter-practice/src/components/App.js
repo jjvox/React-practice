270e0216f4cff660629689159f7692506a25b342
jjvox <cow_boy27@naver.com>
2023-01-06 16:53:35 +0900

    setting / login 완료

---
+++ twitter-practice/src/components/App.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import AppRouter from "components/Router";
+import { auth } from "firebaseServer";
+import { onAuthStateChanged } from "firebase/auth";
+
+function App() {
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+  return (
+    <>
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}
+      <footer>&copy; {new Date().getFullYear()} twitter</footer>
+    </>
+  );
+}
+
+export default App;
